8062fafa7b6fefc7988b8f66c02514128127ca90
eduardo <daw2eespinosa@iesjoanramis.org>
2015-11-11 11:05:08 +0100

    Ejemplo 5.2

--- app/src/main/java/com/eduardoespinosa/aplicacion1/Localizacion.java
+++ app/src/main/java/com/eduardoespinosa/aplicacion1/Localizacion.java
@@ -3,6 +3,9 @@ package com.eduardoespinosa.aplicacion1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,6 +23,23 @@ public class Localizacion extends AppCompatActivity {
                 lanzarSobre(null);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater infl = getMenuInflater();
+        infl.inflate(R.menu.menulocalizacion, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.sobre : lanzarSobre(null); break;
+
+        }
+        return true;
     }
 
     public void lanzarSobre(View view) {
