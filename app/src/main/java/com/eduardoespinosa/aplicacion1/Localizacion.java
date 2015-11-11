4d5398d793d3520689bf0c2d50ad8238ca0e39f0
eduardo <daw2eespinosa@iesjoanramis.org>
2015-11-11 10:34:56 +0100

    Ejemplo 5.1

--- app/src/main/java/com/eduardoespinosa/aplicacion1/Localizacion.java
+++ app/src/main/java/com/eduardoespinosa/aplicacion1/Localizacion.java
@@ -1,7 +1,10 @@
 package com.eduardoespinosa.aplicacion1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Localizacion extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Localizacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button bSobre = (Button) findViewById(R.id.botonAcerca);
+        bSobre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lanzarSobre(null);
+            }
+        });
+    }
+
+    public void lanzarSobre(View view) {
+        Intent i = new Intent(this, Sobre.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/eduardoespinosa/aplicacion1/Sobre.java
@@ -0,0 +1,15 @@
+package com.eduardoespinosa.aplicacion1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by eduardo on 11/11/15.
+ */
+public class Sobre extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sobre);
+    }
+}
